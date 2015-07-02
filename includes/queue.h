4b285550f81d5b1d64684ae32fca5b1129f90ca0
tcd4 <tcd4@njit.edu>
2015-07-02 15:50:17 -0400

    queue.h - added

---
+++ includes/queue.h
@@ -0,0 +1,33 @@
+#ifndef __QUEUE__
+#define __QUEUE__
+
+
+/**
+ * @file queue.h
+ *
+ * @brief contains support for queues
+ *
+ * Queue Type - Queue
+ *
+ * Create Function - New_Queue()
+ * Push/Pop Functions - Queue_Push(), Queue_Pop()
+ * Util Functions - Clear_Queue(), Is_Queue_Empty(), Duplicate_Queue()
+ * Free Function - Free_Queue()
+ */
+
+
+#include <string.h>
+#include "stdmacros.h"
+#include "stdtypes.h"
+#include "platforms.h"
+#include "boolean.h"
+#include "linked_list.h"
+
+
+START_DECLS
+
+
+END_DECLS
+
+
+#endif
