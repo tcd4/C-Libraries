436983853e30cfb40ed749574014a6dc832b2dc5
tcd4 <tcd4@njit.edu>
2015-07-05 13:57:21 -0400

    stack.h - added

---
+++ includes/stack.h
@@ -0,0 +1,23 @@
+#ifndef __STACK__
+#define __STACK__
+
+
+/**
+ * @file stack.h
+ *
+ * @brief contains support for stacks
+ *
+ * Stack Type - Stack
+ *
+ * Create Function - New_Stack()
+ * Push/Pop Functions - Stack_Push(), Stack_Pop()
+ * Util Functions - Clear_Stack(), Is_Stack_Empty(), Duplicate_Stack()
+ * Free Function - Free_Stack()
+ *
+ * WARNING - STACKS SHOULD ONLY CONTAIN 1 DATA TYPE
+ */
+
+
+
+
+#endif
