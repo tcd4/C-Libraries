01ff4d1a2c6bdff5029d5dadc78a691bb5a36216
tcd4 <tcd4@njit.edu>
2015-07-05 14:10:34 -0400

    stack.h - updated - Stack struc added, functions added

--- includes/stack.h
+++ includes/stack.h
@@ -18,6 +18,89 @@
  */
 
 
+#include <string.h>
+#include "stdmacros.h"
+#include "stdtypes.h"
+#include "platforms.h"
+#include "boolean.h"
+#include "linked_list.h"
+
+
+START_DECLS
+
+
+/**< the stack struct */
+typedef struct
+{
+    List       *start;                       /**< the first element in the stack */
+    List       *end;                         /**< the last element in the stack */ 
+    uint32     length;                       /**< the number of elements in the stack */
+    size_t     alloc;                        /**< the allocation size of the data */
+
+    void       ( *Free )( dataptr data );    /**< function pointer for the data's custom free function */
+}Stack;
+
+
+/**
+ * @brief creates a new Stack
+ *
+ * @param size the allocation size of the type of data the Stack will hold
+ * @param Free a pointer to the data's custom free function
+ *
+ * @return a pointer to the new Stack
+ */
+Stack* New_Stack( size_t size, void ( *Free )( dataptr data ) );
+
+/**
+ * @brief adds data onto the end of a Stack
+ *
+ * @param stack the Stack to push onto
+ * @param data the data to push onto the Stack
+ */
+void Push_Stack( Stack *stack, dataptr data );
+
+/**
+ * @brief retrieves the data from the last element of th Stack and removes it from the Stack
+ *
+ * @param stack the Stack to pop off of
+ *
+ * @return a pointer to the popped data
+ */
+dataptr Pop_Stack( Stack *stack );
+/**
+ * @brief clears all the data from a Stack
+ *
+ * @param stack the Stack to clear
+ */
+void Clear_Stack( Stack *stack );
+
+/**
+ * @brief checks if a Stack is empty
+ *
+ * @param stack the Stack to check
+ *
+ * @return TRUE if the Stack if empty, False if else
+ */
+Bool Is_Stack_Empty( Stack *stack );
+
+/**
+ * @brief duplicates a Stack
+ *
+ * @param stack the Stack to duplicated
+ *
+ * @return a pointer to the duplicate Stack
+ */
+Stack* Duplicate_Stack( Stack *stack );
+
+/**
+ * @brief frees the memory of a Stack
+ *
+ * @param stack the Stack to free
+ */
+void Free_Stack( Stack **stack );
+
+
+END_DECLS
 
 
 #endif
