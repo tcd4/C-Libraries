0bc3499636ba5d61c29d770de324a90a66a226b3
tcd4 <tcd4@njit.edu>
2015-07-05 14:15:55 -0400

    stack.c - updated - New_Stack ready for testing

--- types/stack.c
+++ types/stack.c
@@ -1,4 +1,50 @@
 #include "stack.h"
 
 
+Stack* New_Stack( size_t size, void ( *Free )( dataptr data ) )
+{
+    Stack *new;
+
+    new = C_New( Stack, 1 );
+    Return_Val_If_Fail( new, NULL );
+
+    new->start = NULL;
+    new->end = NULL;
+    new->length = 0;
+    new->alloc = size;
+    new->Free = Free;
+
+    return new;
+}
+
+/*
+void Push_Stack( Stack *stack, dataptr data )
+{
+}
+
+
+dataptr Pop_Stack( Stack *stack )
+{
+}
+
+
+void Clear_Stack( Stack *stack )
+{
+}
+
+
+Bool Is_Stack_Empty( Stack *stack )
+{
+}
+
+
+Stack* Duplicate_Stack( Stack *stack )
+{
+}
+
+
+void Free_Stack( Stack **stack )
+{
+}
+*/
 /*eof*/
