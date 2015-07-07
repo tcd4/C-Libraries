c6d3427219861659e38f04e3a45db942624d62ca
tcd4 <tcd4@njit.edu>
2015-07-07 14:20:33 -0400

    dynamic_array.h - added

---
+++ includes/dynamic_array.h
@@ -0,0 +1,33 @@
+#ifndef __DYNAMIC_ARRAY__
+#define __DYNAMIC_ARRAY__
+
+
+/**
+ * @file dynamic_array.h
+ *
+ * @brief contains support for dynamically sized arrays
+ *
+ * Dynamic Array Type - DArray
+ *
+ * Create Function - New_DArray()
+ * Insert Functions - Insert_Into_DArray(), Prepend_To_DArray(), Append_To_DArray()
+ * Util Functions - Element_At(), Resize_DArray(), Remove_Element_At(), Clear_DArray()
+ * Free Function - Free_DArray()
+ *
+ * WARNING: DARRAYS SHOULD ONLY CONTAIN 1 DATA TYPE
+ * WARNING: USERS WILL BE RESPONSIBLE FOR ALLOCATING MEMORY FOR NON-STANDARD C DATA TYPES
+ * WARNING: MAKE SURE YOU HAVE NO POINTERS TO DARRAY DATA WHEN FREEING A DARRAY
+ */
+
+
+#include <string.h>
+#include "stdmarocs.h"
+#include "stdfuncs.h"
+#include "platforms.h"
+#include "boolean.h"
+
+
+
+
+
+#endif
