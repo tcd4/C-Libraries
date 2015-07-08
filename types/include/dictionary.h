5016f29cc1af383fa77988986ffa47fae2b0c0c4
tcd4 <tcd4@njit.edu>
2015-07-08 16:57:51 -0400

    dictionary.h - added

---
+++ types/include/dictionary.h
@@ -0,0 +1,34 @@
+#ifndef __DICTIONARY__
+#define __DICTIONARY__
+
+
+/**
+ * @file dictionary.h
+ *
+ * @brief contains support for dictionaries
+ *
+ * Dictionary Type - KeyChain
+ *
+ * Create Function - New_KC()
+ * Free Function - Free_KC()
+ */
+
+
+#include "stdmacros.h"
+#include "stdtypes.h"
+#include "stdfuncs.h"
+#include "platforms.h"
+#include "boolean.h"
+#include "linked_list.h"
+#include "doubly_linked_list.h"
+#include "queue.h"
+#include "stack.h"
+
+
+START_DECLS
+
+
+END_DECLS
+
+
+#endif
