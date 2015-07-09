80bafe3122f12f0377aeeba8f8820938e76d24ef
tcd4 <tcd4@njit.edu>
2015-07-09 14:58:57 -0400

    dictionary.h - updated - Charm struct updated, functions added

--- types/include/dictionary.h
+++ types/include/dictionary.h
@@ -28,11 +28,18 @@ START_DECLS
 /**< the key/value type */
 typedef struct Charm_s
 {
-    dataptr         name;          /**< the key */
-    CTypes          nameType;      /**< the key type */
+    dataptr         key;           /**< the key */
+    CTypes          keyType;       /**< the key type */
+
+    CompareNotify   keyCompare;    /**< the compare function for the key */
+    CloneNotify     keyClone;      /**< the duplicate function for the key */
+    FreeNotify      keyDestroy;    /**< the free function for the key */
+
     dataptr         val;           /**< the value */
     CTypes          valType;       /**< the value type */
-    FreeNotify      destroy;       /**< the free function to use */
+
+    CloneNotify     valClone;      /**< the duplicate function for the value */
+    FreeNotify      valDestroy;    /**< the free function for the value */  
 }Charm;
 
 
@@ -42,6 +49,24 @@ typedef struct Bracelet_s
     DList      *charms;       /**< a DList of charms to hold key/value pairs */
     uint32     count;         /**< the number of charms in the DList */
 }Bracelet;
+
+
+Bracelet* New_Bracelet();
+Charm* New_Charm( dataptr key, CTypes keyType, dataptr val, CTypes valType );
+
+void Set_Charm_Key_Funcs( Charm *charm, CompareNotify compare, CloneNotify clone, FreeNotify destroy );
+void Set_Charm_Val_Funcs( Charm *charm, CloneNotify clone, FreeNotify destroy );
+
+Bool Add_Charm( Bracelet *bracelet, Charm *charm );
+void Remove_Charm( Bracelet *bracelet, Charm *charm );
+Charm* Find_Charm( Bracelet *bracelet, dataptr data );
+
+void Clear_Bracelet( Bracelet *bracelet );
+Bracelet* Duplicate_Bracelet( Bracelet *bracelet );
+Charm* Duplicate_Charm( Charm *charm );
+
+void Free_Charm( Charm **charm );
+void Free_Bracelet( Bracelet **bracelet );
 
 
 END_DECLS
